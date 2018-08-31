55899360e29c344c446dc4776e08f063998f688f
deito <deito.alvarez@gmail.com>
2018-08-31 17:49:33 -0500

    Clase 21. Insertando listados en plantillas

--- src/main/java/com/udemy/backendninja/controller/ExampleController.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController.java
@@ -1,5 +1,8 @@
 package com.udemy.backendninja.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +24,7 @@ public class ExampleController {
 	@GetMapping("/exampleString")
 	//@RequestMapping(value="/exampleString", method=RequestMethod.GET)
 	public String exampleString(Model model) {
-		model.addAttribute("person", new Person("Jon",23));
+		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;
 	}
 	
@@ -31,8 +34,17 @@ public class ExampleController {
 	@RequestMapping(value="/exampleMAV", method=RequestMethod.GET)
 	public ModelAndView exampleMAV() {
 		ModelAndView mav= new ModelAndView(EXAMPLE_VIEW);
-		mav.addObject("person", new Person("Mikel",30));
+		mav.addObject("people", getPeople());
 		return mav;
 		//return new ModelAndView(EXAMPLE_VIEW);
+	}
+	
+	private List<Person> getPeople(){
+		List<Person> people = new ArrayList<>();
+		people.add(new Person("Jon",23));
+		people.add(new Person("Mikel",30));
+		people.add(new Person("Eva",43));
+		people.add(new Person("Peter",18));
+		return people;
 	}
 }
