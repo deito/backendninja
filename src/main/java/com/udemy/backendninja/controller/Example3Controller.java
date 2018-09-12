c498c69c479bef42f70e82e52023d57729bd4457
deito <deito.alvarez@gmail.com>
2018-09-11 21:58:14 -0500

    Clase 25. Redirecciones

--- src/main/java/com/udemy/backendninja/controller/Example3Controller.java
+++ src/main/java/com/udemy/backendninja/controller/Example3Controller.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import com.udemy.model.Person;
 
@@ -15,6 +16,18 @@ import com.udemy.model.Person;
 public class Example3Controller {
 	public static final String FORM_VIEW = "form";
 	public static final String RESULT_VIEW = "result";
+	
+	//Redireccion: forma 1
+	/*
+	@GetMapping("/")
+	public String redirect() {
+		return "redirect:/example3/showform";
+	}*/
+	
+	@GetMapping("/")
+	public RedirectView redirect() {
+		return new RedirectView("/example3/showform");
+	}
 
 	@GetMapping("/showform")
 	public String showForm(Model model) {
