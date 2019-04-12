d2644ac64ac681abe33c3dd3855e2bb5ee17008f
deito <deito.alvarez@gmail.com>
2019-04-12 12:26:07 -0500

    Clase 41. Creando e integrando todas las capas - El controller

--- src/main/java/com/udemy/service/impl/CourseServiceImpl.java
+++ src/main/java/com/udemy/service/impl/CourseServiceImpl.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.udemy.entity.Course;
 import com.udemy.repository.CourseJpaRepository;
 import com.udemy.service.CourseService;
 
+@Service("courseServiceImpl")
 public class CourseServiceImpl implements CourseService {
 
 	@Autowired
