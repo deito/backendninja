6cc299fca8eecf2a8f89a2654de36b6d1bf338ca
deito <deito.alvarez@gmail.com>
2019-04-25 18:38:12 -0500

    Clase 66. Obteniendo el usuario autenticado en el controlador

--- src/main/java/com/udemy/converter/TestCrypt.java
+++ src/main/java/com/udemy/converter/TestCrypt.java
@@ -7,6 +7,8 @@ public class TestCrypt {
 	public static void main(String[] args) {
 		BCryptPasswordEncoder pe = new BCryptPasswordEncoder();
 		System.out.println(pe.encode("user"));
+		
+		System.out.println(pe.encode("diego"));
 
 	}
 
