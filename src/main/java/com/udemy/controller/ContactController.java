c003d4aad46b6c656e2d0a91b4394829d2d882cc
deito <deito.alvarez@gmail.com>
2019-04-25 18:14:11 -0500

    Clase 65. Securizando los controladores

--- src/main/java/com/udemy/controller/ContactController.java
+++ src/main/java/com/udemy/controller/ContactController.java
@@ -4,6 +4,7 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +33,9 @@ public class ContactController {
 		return "redirect:/contacts/showcontacts";
 	}
 
+	@PreAuthorize("hasRole('ROLE_USER')")
 	@GetMapping("/contactform")
-	private String redirectContactForm(@RequestParam(name="id", required=false) int id,
+	public String redirectContactForm(@RequestParam(name="id", required=false) int id,
 			Model model) {
 		ContactModel contact = new ContactModel();
 		if(id != 0) {
